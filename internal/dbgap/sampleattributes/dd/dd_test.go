30caf161a3334f55e7cf070cbdbec33787e3eb09
John Frommeyer <johnfr@seas.upenn.edu>
2026-09-09 15:15:05 -0400

    Fix test for removed DD UNIQUEKEY column.

--- internal/dbgap/sampleattributes/dd/dd_test.go
+++ internal/dbgap/sampleattributes/dd/dd_test.go
@@ -16,14 +16,13 @@ func TestSpec_EncodedValueVariable(t *testing.T) {
 	require.Len(spec.Rows, 1)
 
 	row := spec.Rows[0]
-	// header is VARNAME, VARDESC, TYPE, UNIQUEKEY, VALUES; VALUES expands to one cell per encoded value
+	// header is VARNAME, VARDESC, TYPE, VALUES; VALUES expands to one cell per encoded value
 	require.Equal(models.IsTumorVar.Name, row[0])
 	require.Equal(models.IsTumorVar.Description, row[1])
 	require.Equal(models.IsTumorVar.Type, row[2])
-	require.Equal("", row[3])
-	require.Equal("Y=Is tumor", row[4].(dd.EncodedValue).String())
-	require.Equal("N=Is not a tumor", row[5].(dd.EncodedValue).String())
-	require.Equal("UNK=Tumor status unknown", row[6].(dd.EncodedValue).String())
+	require.Equal("Y=Is tumor", row[3].(dd.EncodedValue).String())
+	require.Equal("N=Is not a tumor", row[4].(dd.EncodedValue).String())
+	require.Equal("UNK=Tumor status unknown", row[5].(dd.EncodedValue).String())
 }
 
 func TestSpec_StringVariable(t *testing.T) {
@@ -31,7 +30,7 @@ func TestSpec_StringVariable(t *testing.T) {
 
 	row := spec.Rows[0]
 	// no VALUES since AnalyteTypeVar has none
-	assert.Len(t, row, 4)
+	assert.Len(t, row, 3)
 	assert.Equal(t, models.AnalyteTypeVar.Name, row[0])
 	assert.Equal(t, models.AnalyteTypeVar.Description, row[1])
 	assert.Equal(t, models.AnalyteTypeVar.Type, row[2])
